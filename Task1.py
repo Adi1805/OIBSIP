6e4956b7c045bc90d9f5774ee47470ce6aad13db
Adi1805 <146560732+Adi1805@users.noreply.github.com>
2024-08-04 17:48:56 +0530

    Update Task1.py

--- Task1.py
+++ Task1.py
@@ -1,3 +1,6 @@
+#Task1-Create a basic voice assistant that can perform simple tasks based on voice commands.
+#Implement features like responding to "Hello" and providing predefined responses, telling the time or date, and searching the web for information based on user queries.
+
 import speech_recognition as sr
 import pyttsx3
 import datetime
